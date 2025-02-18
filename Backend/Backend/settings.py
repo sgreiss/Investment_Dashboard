42ab4af7e4d0e59d3a3586e66107132d77946719
sgreiss <greiss.spencer@gmail.com>
2025-02-18 13:36:53 -0500

    updated with Postgres user settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'investment_dashboard',
+        'USER': 'postgres',
+        'PASSWORD': 'sgbbb@11',
+        'HOST': 'localhost',
+        'PORT': '8000',
     }
 }
 
